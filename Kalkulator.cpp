da200e3c013943faf5bab097d588b09b87ca3167
saptuge <79316336+saptuge@users.noreply.github.com>
2021-03-05 16:23:05 +0700

    Add files via upload

---
+++ Kalkulator.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  int a,b,J,KU,KA,BA;
+  printf("input angka pertama : \n");
+  scanf("%d",&a);
+  printf("input angka kedua : \n");
+  scanf("%d",&b);
+    J=a+b;
+    KU=a-b;
+    KA=a*b;
+    BA=a/b;
+    printf("\nHasil Penjumlahan    : %d" ,J);
+    printf("\nHasil Pengurangan   : %d" ,KU);
+    printf("\nHasil Perkalian.        : %d" ,KA);
+    printf("\nHasil Pembagian.      : %d" ,BA);
+    getch();
+    return 0;
+}
